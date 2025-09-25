bf5b8ca88205cc6eb43b57540f1d51d069339200
nimble-sloth <karishevorders@gmail.com>
2025-09-24 20:40:44 -0400

    HtmlTools

--- common/src/main/java/com/example/template/common/utils/HtmlTools.java
+++ common/src/main/java/com/example/template/common/utils/HtmlTools.java
@@ -1,5 +1,8 @@
-﻿package com.example.template.common.utils;
+package com.example.template.common.utils;
+
 public final class HtmlTools {
   private HtmlTools(){}
-  public static String wrap(String body){ return "<html><body>"+body+"</body></html>"; }
+  public static String wrap(String body){
+    return "<html><body>" + body + "</body></html>";
+  }
 }
